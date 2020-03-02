574de5f3665c259597a7b172a3d23a9722b39a80
tkctkc <38493534+Takeshi-005@users.noreply.github.com>
2020-03-02 18:32:10 +0900

    Update config.js

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,24 @@
 import { configure, addDecorator, addParameters } from '@storybook/react';
-import { DocsPage } from '@storybook/addon-docs/blocks';
+// import { DocsPage } from '@storybook/addon-docs/blocks';
+import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
 
-const req = require.context('../src/stories/', true, /.(story|stories).tsx$/);
 
 function loadStories() {
-  addParameters({
-    docs: DocsPage,
-  });
+  const req = require.context('../src/components/', true, /.(story|stories).tsx$/);
+  // addParameters({docs: DocsPage});
+  addDecorator(withInfo);
   addDecorator(withKnobs);
+  addParameters({
+    info: {
+      // styles: {
+      //   infoBody: beautifyStory.infoBody,
+      //   infoStory: beautifyStory.infoStory,
+      // },
+      inline: true,
+      header: true,
+    }
+  })
   req.keys().forEach(req);
 }
 
